a09c3b1ff754e2e3688a725d25104782093ffcea
James Scrivano <97805519+jscrivano@users.noreply.github.com>
2022-01-15 13:29:00 -0800

    thumbnail feature

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,29 @@
 (function () {
-    alert("invoked main");
+    var IMAGE_STYLE = "max-height: 80px; max-width: 80px";
+    var isImage = function (url) {
+        var low = url.toLocaleLowerCase();
+        return low.indexOf(".jpg") > 0 ||
+            low.indexOf(".jpeg") > 0 ||
+            low.indexOf(".png") > 0 ||
+            low.indexOf(".gif") > 0;
+    };
+    var aElems = document.getElementsByTagName("a");
+    var axs = [];
+    for (var i = 0; i < aElems.length; i++) {
+        axs = axs.concat(aElems.item(i));
+    }
+    // thumbnail images
+    axs.forEach(function (e) {
+        var ahref = e.href;
+        if (isImage(ahref)) {
+            var imgThumb = document.createElement("img");
+            imgThumb.src = ahref;
+            imgThumb.setAttribute('style', IMAGE_STYLE);
+            var imgThumbLink = document.createElement("a");
+            imgThumbLink.href = ahref;
+            imgThumbLink.appendChild(imgThumb);
+            e.parentElement.prepend(imgThumbLink);
+        }
+    });
 })();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
 (function() {
-    alert("invoked main");
+    const IMAGE_STYLE = "max-height: 80px; max-width: 80px";
+
+    const isImage = function(url : string) : boolean {
+        const low = url.toLocaleLowerCase();
+        return low.indexOf(".jpg") > 0 ||
+            low.indexOf(".jpeg") > 0 ||
+            low.indexOf(".png") > 0 ||
+            low.indexOf(".gif") > 0;
+    }
+    
+    const aElems = document.getElementsByTagName("a");
+    var axs = [];
+    for(var i=0; i < aElems.length; i++) {
+        axs = axs.concat(aElems.item(i));
+    }
+
+    // thumbnail images
+    axs.forEach((e) => {
+        const ahref = e.href;
+        if (isImage(ahref)) {
+            const imgThumb = document.createElement("img");
+            imgThumb.src = ahref;
+            imgThumb.setAttribute('style', IMAGE_STYLE);
+
+            const imgThumbLink = document.createElement("a");
+            imgThumbLink.href = ahref;
+            imgThumbLink.appendChild(imgThumb);
+
+            e.parentElement.prepend(imgThumbLink);
+        }
+    });
+
+
+
 })();
